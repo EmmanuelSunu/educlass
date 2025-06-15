a9e24d61411c40e0fa191c7d08ba9a33c4dd159d
Severin Asutani <50738523+Awedoba@users.noreply.github.com>
2025-06-15 00:16:32 +0000

    set up the migrations somewhat

---
+++ backend/database/migrations/2025_06_14_231428_create_courses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('program_id');
+            $table->string('level');
+            $table->string('code');
+            $table->string('name');
+            $table->string('description');
+            $table->string('credits');
+            $table->string('duration');
+            $table->string('status');
+            $table->string('lecturer_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
